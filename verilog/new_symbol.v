abbd278b3ae1ad69361198d05a8be16b4bb659fe
andrew <afoote97@gmail.com>
2022-08-27 23:26:01 -0700

    add new symbol block and tb

---
+++ verilog/new_symbol.v
@@ -0,0 +1,27 @@
+`default_nettype none
+module new_symbol(
+    input [7:0] state_in,
+    input s2,
+    input s1,
+    input s0,
+    output z2,
+    output z1,
+    output z0
+);
+
+wire a,b,c,d,e,f,g,h;
+
+assign a = state_in[0];
+assign b = state_in[1];
+assign c = state_in[2];
+assign d = state_in[3];
+assign e = state_in[4];
+assign f = state_in[5];
+assign g = state_in[6];
+assign h = state_in[7];
+
+assign z2 = ((~s2) & (b | (d & s0))) | c | (e & (s0 | s1)) | (f & (~(s2 | s1)));
+assign z1 = (a & (~s2)) | (d & (s2 | s1) & (~s0)) | (e & s2 & (~s0));
+assign z0 = (s0 & ((a & s2) | (~a))) | (h & s1);
+
+endmodule
---
+++ verilog/tb_new_symbol.v
@@ -0,0 +1,46 @@
+module tb_new_symbol();
+
+reg [7:0] state;
+reg [2:0] sym;
+wire [2:0] new_sym;
+
+new_symbol newsym(
+    .state_in(state),
+    .s2(sym[2]),
+    .s1(sym[1]),
+    .s0(sym[0]),
+    .z2(new_sym[2]),
+    .z1(new_sym[1]),
+    .z0(new_sym[0])
+);
+
+integer i;
+integer j;
+initial begin
+    $dumpvars;
+    for (i = 0; i < 8; i = i + 1) begin
+        state = 1 << i;
+        $display("==== state 0x%x %c", state, "A" + $clog2(state));
+        sym = 3'b000;
+        #10;
+        $display("sym %b -> new sym %b", sym, new_sym);
+        #10;
+        sym = 3'b001;
+        #10;
+        $display("sym %b -> new sym %b", sym, new_sym);
+        #10;
+        sym = 3'b010;
+        #10;
+        $display("sym %b -> new sym %b", sym, new_sym);
+        #10;
+        sym = 3'b100;
+        #10;
+        $display("sym %b -> new sym %b", sym, new_sym);
+        #10;
+        sym = 3'b101;
+        #10;
+        $display("sym %b -> new sym %b", sym, new_sym);
+    end
+end
+
+endmodule
