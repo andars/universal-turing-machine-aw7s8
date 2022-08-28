ac920a5e2791b85f601714693e76097361a22c0b
andrew <afoote97@gmail.com>
2022-08-28 12:36:23 -0700

    add missing user module

---
+++ src/user_module_341174563322724948.v
@@ -0,0 +1,12 @@
+`default_nettype none
+
+module user_module_341174563322724948(
+  input [7:0] io_in,
+  output [7:0] io_out
+);
+
+// TODO
+assign io_out[0] = ~io_in[0];
+assign io_out[1] = ~io_in[0];
+
+endmodule
