150d33fb76b0d65ea36a792db5fc7d5f8503fa15
andrew <afoote97@gmail.com>
2022-08-29 21:56:28 -0700

    fix new_symbol logic
    (0x08, 101) should have new symbol of 101, not 001.
    Fix z2 equation so it does.

--- verilog/new_symbol.v
+++ verilog/new_symbol.v
@@ -20,7 +20,7 @@ assign f = state_in[5];
 assign g = state_in[6];
 assign h = state_in[7];
 
-assign z2 = ((~s2) & (b | (d & s0))) | c | (e & (s0 | s1)) | (f & (~(s2 | s1)));
+assign z2 = ((~s2) & b) | (d & s0) | c | (e & (s0 | s1)) | (f & (~(s2 | s1)));
 assign z1 = (a & (~s2)) | (d & (s2 | s1) & (~s0)) | (e & s2 & (~s0));
 assign z0 = (s0 & ((a & s2) | (~a))) | (h & s1);
 
